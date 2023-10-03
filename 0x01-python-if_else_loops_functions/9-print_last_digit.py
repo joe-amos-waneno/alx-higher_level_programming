91eed4a9f1853e19da7a130f7639a337a4936a0b
joe-amos-waneno <waneno.amos@gmail.com>
2023-10-03 09:45:52 -0700

    last num

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        number = ((-1) * number) % 10
-    else:
-        number = number % 10
-
-
-    print("{:d}".format(number), end="")
-    return number
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
