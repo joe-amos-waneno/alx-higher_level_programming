ab7a77fd5d1e1f3af18704779d17b552b131fc99
joe-amos-waneno <waneno.amos@gmail.com>
2023-10-09 14:33:34 -0700

    MATRIX

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@
 
 def print_matrix_integer(matrix=[[]]):
     for q in matrix:
-        for col in row:
+        for m in q:
             print("{:d}".format(m), end=" " if m != q[-1] else "")
         print()
