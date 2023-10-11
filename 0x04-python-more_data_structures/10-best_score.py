c2998d61bb83554bdfa615f032e330fa7c209b14
joe-amos-waneno <waneno.amos@gmail.com>
2023-10-11 08:47:20 -0700

    Best scores

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
-def multiply_list_map(my_list=[], number=0):
-    return (list(map((lambda i: i * number), my_list)))
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
