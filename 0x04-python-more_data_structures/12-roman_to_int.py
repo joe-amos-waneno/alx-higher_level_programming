7650df60276cd12f5b4fa65f126201ccd74dc836
joe-amos-waneno <waneno.amos@gmail.com>
2023-10-11 09:16:12 -0700

    Roman to Int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -17,7 +17,7 @@ def roman_to_int(roman_string):
         return 0
 
     rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(rom_n.keys())
+    list_keys = list(rom_num.keys())
 
     number = 0
     roman = 0
