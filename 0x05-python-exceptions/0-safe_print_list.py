b071bcf9e49fda5dc1d4bb5402b69cb6695219b2
joe-amos-waneno <waneno.amos@gmail.com>
2023-10-23 13:42:08 -0700

    print list 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,6 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
-    q = 0
-    for l in range(x):
+def safe_print_integer(value):
         try:
             print("{}".format(my_list[l]), end="")
             q += 1
